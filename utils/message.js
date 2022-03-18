23070687f843a91b7461c628251e4774ff74dd14
Aman Agnihotri <69078309+amanagnihotri1@users.noreply.github.com>
2022-03-19 00:06:29 +0530

    Update message.js

--- utils/message.js
+++ utils/message.js
@@ -3,7 +3,7 @@ function formatMessage(user,text){
 return {
         user,
         text,
-        time:moment().format('h:mm a')
+        time:moment().format('LT')
     };
 }
 module.exports=formatMessage;
