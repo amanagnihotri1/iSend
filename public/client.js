b352d4075337219dd295afa298742ab9cae1bff5
Aman Agnihotri <69078309+amanagnihotri1@users.noreply.github.com>
2022-03-18 23:53:02 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 const chatForm=document.getElementById("uf");
 const f=document.getElementById("formf");
-const socket= io("http://localhost:5000");
+const socket= io("ws://.aman4gnihotri.herokuapp.com/socket.io/?EIO=4&transport=websocket");
 const btn=document.getElementById("leavebtn");
 const{ username,roominput }= Qs.parse(window.location.search,{
     ignoreQueryPrefix:true   
